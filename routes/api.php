e71a9da8be9037872222094a5f4c64d08078b86b
aqilaakmalia <aqilaakmalia98@gmail.com>
2022-10-21 11:14:46 +0700

    menambahkan routing

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-Use App\Http\Controllers\API\DesainerController;
+use App\Http\Controllers\API\DesainerController;
 use App\Http\Controllers\DocumentController;
 use App\Http\Controllers\API\OrderController;
 
@@ -17,6 +17,8 @@ use App\Http\Controllers\API\OrderController;
 |
 */
 
+header('Access-Control-Allow-Origin: *'); 
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 }); 
@@ -27,7 +29,14 @@ Route::resource('produk', App\Http\Controllers\API\ProdukController::class);
 Route::resource('desainer', App\Http\Controllers\API\DesainerController::class);
 Route::resource('project', App\Http\Controllers\API\ProjectController::class);
 Route::resource('order', App\Http\Controllers\API\OrderController::class);
-Route::resource('user', App\Http\Controllers\API\UserController::class);
+Route::resource('keranjang', App\Http\Controllers\API\KeranjangController::class);
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::post('login', [ApiController::class, 'login']);
+Route::post('user', 'App\Http\Controllers\API\UserController@store');
 
 // route khusus
 Route::get('produk_desc_rating', 'App\Http\Controllers\API\ProdukController@desc_rating');
@@ -37,9 +46,13 @@ Route::get('document', 'App\Http\Controllers\API\DocumentController@index');
 Route::get('product_filter_category/{nama_kategori}', 'App\Http\Controllers\API\ProdukController@filter_kategori');
 Route::post('store-file', 'App\Http\Controllers\API\DocumentController@store');
 Route::get('preorder/{id_user}', 'App\Http\Controllers\API\ProjectController@riwayat');
-Route::get('get-user/{username}', 'App\Http\Controllers\API\UserController@getUser');
+Route::post('get-user', 'App\Http\Controllers\API\UserController@getUser');
+Route::get('pengalaman_desainer', 'App\Http\Controllers\API\PengalamanDesainerController@index');
+Route::get('pengalaman_konveksi', 'App\Http\Controllers\API\PengalamanKonveksiController@index');
+Route::get('atasan', 'App\Http\Controllers\API\AtasanController@index');
+Route::get('bawahan', 'App\Http\Controllers\API\BawahanController@index');
 
-//Request Project
+//Project
 Route::get('requestProjectDesainer', 'App\Http\Controllers\API\RequestProjectDesainerController@index');
 Route::post('createRequestProjectDesainer', 'App\Http\Controllers\API\RequestProjectDesainerController@store');
 Route::get('requestProjectKonveksi', 'App\Http\Controllers\API\RequestProjectKonveksiController@index');
