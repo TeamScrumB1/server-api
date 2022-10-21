9802b5715dfc6c72db922d93cd6693c748775129
aqilaakmalia <aqilaakmalia98@gmail.com>
2022-10-21 11:16:15 +0700

    menambahkan controller & model keranjang

--- app/Models/Konveksi.php
+++ app/Models/Konveksi.php
@@ -7,12 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Konveksi extends Model
 {
+    use HasFactory;
+
     protected $guarded = [];
     protected $cast =[
         'id' => 'integer'
     ];
     protected $table ='konveksi';
     protected $fillable =['img_profil','nama','bio','rating','link_wa','link_porto','jmlh_project','id_kategori','id_tarif','id_pengalaman'];
-
-    use HasFactory;
 }
